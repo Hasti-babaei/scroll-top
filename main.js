2905bed2d831b9f4308624d0e76bd31882a05f30
Hasti-babaei <beingbabaei@gmail.com>
2023-10-09 13:05:26 +0330

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let preloader = document.getElementById("preloader");
+  let content = document.getElementById("content");
+
+  preloader.style.display = "none";
+  content.style.display = "block";
+});
+
+const backToTop = document.getElementById("backToTop");
+backToTop.addEventListener("click", function (e) {
+  scrollTop(1000, 500);
+});
+function scrollTop(scroll, duration) {
+  let doc = document.documentElement;
+  let currentTime = duration;
+  let speed = 10;
+
+  let animate = () => {
+    if (currentTime < 0) return;
+    setTimeout(() => {
+      doc.scrollTop -= doc.scrollTop / (currentTime / speed);
+      console.log(doc.scrollTop);
+      currentTime -= speed;
+      animate();
+    }, speed);
+  };
+
+  animate();
+}
+window.addEventListener("scroll", function (e) {
+  if (document.documentElement.scrollTop > 250) {
+    backToTop.style.display = "flex";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
